556f218ee736e00bdfd9772a44f967ad88761ac1
PerlasKurtNiko <123909184+PerlasKurtNiko@users.noreply.github.com>
2023-09-08 09:33:44 +0800

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int num[] = {1,2,3,4,5,6,7,8,9,0};
+   int i, x;
+    for (i=0;i<10;i++)
+        cout<<num[i]<<"Index ["<<i<<"]"<<endl;
+    return 0;
+}
